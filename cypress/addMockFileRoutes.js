ec6bee6739b36e7d5cfe501420d724802be1cdd7
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-01-13 17:08:03 +0200

    addMockFileRoutes: cut out url hash from mock urls, because cy.intercept doesn't pass it.

--- cypress/addMockFileRoutes.js
+++ cypress/addMockFileRoutes.js
@@ -4,9 +4,10 @@ const { isEqual } = require('lodash');
 const MOCKS_ROOT = process.env.CYPRESS_MOCKS_ROOT || 'cypress/mocks';
 
 export const addMockFileRoutes = fileName => {
-  cy.readFile(path.join(MOCKS_ROOT, fileName)).then(data => {
+  cy.readFile(path.join(MOCKS_ROOT, fileName), {log: true}).then(data => {
     data.forEach(({ url, method = 'GET', body, response }) => {
-      cy.intercept(method, url, req => {
+      // cy.intercept doesn't notice url hash, so let's cut it out. 
+      cy.intercept(method, url.split('#')[0], req => {
         // Check that body matches the one from mock if defined
         if (body === undefined || isEqual(body, req.body)) {
           req.reply(response.status, response.body);
